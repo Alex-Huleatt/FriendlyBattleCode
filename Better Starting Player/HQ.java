ddee298000d1ebba755902eb66985f3ad94abd3f
Alex-Huleatt <Ahuleatt@gmail.com>
2015-11-02 12:26:51 -0700

    various

--- Better Starting Player/HQ.java
+++ Better Starting Player/HQ.java
@@ -1,6 +1,10 @@
+import battlecode.common.RobotController;
 
 public class HQ implements Unit {
-	
+
+	public HQ(RobotController rc) {
+
+	}
 
 	public void run() throws Exception {
 
--- Better Starting Player/Soldier.java
+++ Better Starting Player/Soldier.java
@@ -1,5 +1,10 @@
+import battlecode.common.RobotController;
 
 public class Soldier implements Unit {
+
+	public Soldier(RobotController rc) {
+
+	}
 
 	public void run() throws Exception {
 
